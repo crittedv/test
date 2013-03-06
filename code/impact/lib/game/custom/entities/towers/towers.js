c27d4e147857dce60a57b2f1198a93836123d1b5
Devon Crittenden <crittedv@gmail.com>
2013-03-06 12:16:29 -0500

    This is a test.

---
+++ code/impact/lib/game/custom/entities/towers/towers.js
@@ -0,0 +1,17 @@
+ig.module(
+	'game.custom.entities.towers'
+)
+.requires(
+	'impact.entity'
+)
+.defines(function(){
+
+        EntityTower = ig.Entity.extend({
+
+            init: function() {
+
+            }
+        })
+
+
+});
