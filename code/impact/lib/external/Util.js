71b108bc12fcbf2aba264c0eca7dc435550358a8
unknown <jonathangburke@gmail.com>
2013-03-07 18:44:22 -0800

    Add 'external' directory, Util.js, and EntityEnemy as an example for Devon' impact/lib/external/Util.js impact/lib/game/entities/Enemy.js

---
+++ code/impact/lib/external/Util.js
@@ -0,0 +1,9 @@
+
+
+var SarcasmUtil = {
+    "emit" : function() {
+        console.log("This is just a dumb method to illustrate to Devon how I want to do utils." +
+            " ... uh Not!  Yeah sarcasm or maybe not!");
+    }
+}
+
