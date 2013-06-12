7d0d3b7f339d9e086fbb60cbfec0ef2ec3215af8
twisp42 <jonathangburke@gmail.com>
2013-06-11 22:32:32 -0700

    Add TERRAIN enum to Enum.js so that AStar pathing can use it.

--- code/impact/lib/external/Enum.js
+++ code/impact/lib/external/Enum.js
@@ -10,3 +10,13 @@ var ARMOR_TYPE = {
     MEDIUM : "medium",
     HEAVY  : "heavy"
 }
+
+var TERRAIN = {
+    "Land"  : 1,
+    "Water" : 2,
+    "Rock"  : 4,
+
+    1 : "Land",
+    2 : "Water",
+    4 : "Rock"
+};
